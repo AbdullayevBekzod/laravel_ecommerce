209407d9d4b9c9160000ae322bc5fdb2e0e81526
AbdullayevBekzod <abdullayev.bekzod.0403@gmail.com>
2021-12-08 10:33:08 +0500

    day#3 ShopComponent, Seeder, Dynamisation

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -12,6 +12,7 @@
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/font-awesome.min.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.min.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/owl.carousel.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/flexslider.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/chosen.min.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/color-01.css')}}">
@@ -77,6 +78,12 @@
 													<li class="menu-item" >
 														<a title="Dashboard" href="{{ route('admin.dashboard') }}">Dashboard</a>
 													</li>
+													<li class="menu-item">
+														<a href="{{ route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+													</li>
+													<form id='logout-form' method="POST" action="{{ route('logout')}}">
+														@csrf
+													</form>
 												</ul>
 											</li>
 										@else
@@ -87,6 +94,12 @@
 													<li class="menu-item" >
 														<a title="Dashboard" href="{{ route('user.dashboard') }}">Dashboard</a>
 													</li>
+													<li class="menu-item">
+														<a href="{{ route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+													</li>
+													<form id='logout-form' method="POST" action="{{ route('logout')}}">
+														@csrf
+													</form>
 												</ul>
 											</li>
 										@endif
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@ use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ShopComponent;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use Illuminate\Support\Facades\Route;
@@ -30,6 +31,7 @@ Route::get('/cart', CartComponent::class);
 
 Route::get('/checkout', CheckoutComponent::class);
 
+Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
 
 // Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 //     return view('dashboard');
@@ -44,3 +46,4 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function(){
 Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function(){
     Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
 });
+
